58c688a50817de1233208963bf444feccf1f32db
Danieldan5152 <danieldan5152@gmail.com>
2015-03-24 21:38:23 +0200

    All My Work

---
+++ Simple webrowser/Simple Webbrowser take 2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Webbrowser_take_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// THis function is called when the exit menu item is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by Daniel Steed du Plessis.");
+        }
+
+        /// <summary>
+        /// On the click of this button the web control will display the page requested in the text box (by url
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+
+        /// <summary>
+        /// This is the core function that perform all navigation and post processing
+        /// </summary>
+        private void NavigateToPage()
+        {
+            toolStripStatusLabel1.Text = "Navigation has started";
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        /// <summary>
+        /// This is function will fire every single time a key is pushed and released
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // if the keystorke was enter do something
+            if(e.KeyChar == (char)ConsoleKey.Enter )
+            {
+                //NavigateToPage();
+                button1_Click(null, null);
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Navigation Complete";
+
+            foreach( HtmlElement image in webBrowser1.Document.Images )
+            {
+                image.SetAttribute("scr", "http://repokar.com/images/ck_images/file/72e826b0d4258a305b5318c9882da16f.jpg");
+            }
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)( e.CurrentProgress * 100 / e.MaximumProgress);
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+    }
+}
---
+++ Test 1 - Code 1/Test 1 - Code 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Namespace
+namespace My_First_app
+{
+    // Class (contains funcionalty) 
+    class Program
+    {
+        // Function(entry point)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number, any number?");
+            ConsoleKeyInfo keyinfo = Console.ReadKey();
+
+            if (keyinfo.KeyChar == 'a')
+            {
+                Console.WriteLine("That is not a number! KNOCK IT OFF NOW!");
+            }
+            else
+            {
+                Console.WriteLine("Did you type {0}", keyinfo.KeyChar.ToString());
+            }
+        }
+        /// <summary>
+        /// all this function does is print foo to the screen
+        /// </summary>
+        static void PrintFooToScreen() 
+        {
+            Console.WriteLine("Foo)
+        }
+
+    }
+}
